253b15221cbc75514098a37efadae46abda2f124
Balogh85 <Nyami5000@gmail.com>
2024-10-09 14:44:02 +0200

    ok

--- Teszt/Program.cs
+++ Teszt/Program.cs
@@ -9,7 +9,15 @@ namespace Teszt
     class Program
     {
         static void Main(string[] args)
-        {//bármi
+        {
+            int x = 100,
+                y = 11;
+
+            Console.WriteLine($"1. íratás = {x}, x++ = {x++}");
+            Console.WriteLine($"2. íratás = {x}, x++ = {x++}");
+            Console.WriteLine($"1. íratás = {y}, ++y = {++y}");
+            Console.WriteLine($"2. íratás = {y}, ++y = {++y}");
+            Console.ReadKey();
         }
     }
 }
